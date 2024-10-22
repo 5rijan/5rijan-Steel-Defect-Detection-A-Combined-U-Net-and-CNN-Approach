07c8f590be3127708d3e6e73a74b7ea254b65148
Srijan Chaudhary <87299199+5rijan@users.noreply.github.com>
2024-10-23 00:32:28 +1100

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,12 +1,16 @@
 import streamlit as st
-
 from st_pages import add_page_title, get_nav_from_toml
+from pathlib import Path
 
 st.set_page_config(layout="wide")
 
-nav = get_nav_from_toml(
-    ".streamlit/pages.toml"
-)
+# Get the current file's directory
+current_dir = Path(__file__).resolve().parent
+
+# Construct the path to the pages.toml file
+pages_toml_path = current_dir / '.streamlit' / 'pages.toml'
+
+nav = get_nav_from_toml(str(pages_toml_path))
 
 pg = st.navigation(nav)
 
